6fed760d89cbd4d52a72743791d33f88920d6516
zaqw8zaqw8 <zaqw8zaqw8@gmail.com>
2024-11-15 18:26:41 +0900

    커밋 메시지

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,5 +1,28 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {
-  
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('기차 애매'),
+      ),
+      backgroundColor: Colors.grey[200],
+      body: Container(
+        height: 200,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+        ),
+        child: Text('출발역'),
+        c
+    
+        
+
+    
+      )
+      
+      
+    );
+  }
 }
